9fc8025c10e0e44d22ebe7f12ba25fa3d1f1f06d
antonio <cordeiroengenharia@gmail.com>
2026-09-11 23:51:47 -0300

    Fix CORS allow x-admin-password header

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["*"],
+    allow_headers=["*", "x-admin-password"],
     max_age=86400,
 )
 
